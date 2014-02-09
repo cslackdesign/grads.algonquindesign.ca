a7af63c009d1b9fd3df7977e8f040f3dc57bf890
Cassie Slack <cslackdesign@gmail.com>
2014-02-09 17:46:20 -0500

    Creating better landing area

--- 2014/js/app.js
+++ 2014/js/app.js
@@ -1,16 +1,57 @@
-//EXPANDABLE MENU
+//CAROUSEL
 
-/*
-var navBtn = document.querySelector('.nav-btn');
-var navTop = document.querySelector('.nav-top');
+var $items = $('.carousel-items img');
 
-navBtn.addEventListener('click', function (eventObject) {
-	eventObject.preventDefault();
+$('.next').on('click', function () {
+  var current = $items.filter('[data-state="current"]').index();
+  var next = current + 1;
 
-	if (navTop.getAttribute('data-state') == 'open') {
-		navTop.setAttribute('data-state', 'closed');
-	} else {
-		navTop.setAttribute('data-state', 'open');
-	}
-}, false);
-*/
+  if (next >= $items.length) {
+    next = 0;
+  }
+
+  $items.eq(next).attr('data-state', 'incoming').css('left', '100%').animate({
+      left: 0
+    },
+    250,
+    function () {
+      $items.eq(next).attr('data-state', 'current');
+    }
+  );
+
+  $items.eq(current).animate({
+      left: $items.eq(current).width() * -1
+    },
+    250,
+    function () {
+      $items.eq(current).attr('data-state', '');
+    }
+  );
+});
+
+$('.prev').on('click', function () {
+  var current = $items.filter('[data-state="current"]').index();
+  var prev = current - 1;
+
+  if (prev < 0) {
+    prev = $items.length - 1;
+  }
+
+  $items.eq(prev).attr('data-state', 'incoming').css('left', $items.eq(current).width() * -1).animate({
+      left: 0
+    },
+    250,
+    function () {
+      $items.eq(prev).attr('data-state', 'current');
+    }
+  );
+
+  $items.eq(current).animate({
+      left: $items.eq(current).width()
+    },
+    250,
+    function () {
+      $items.eq(current).attr('data-state', '');
+    }
+  );
+});
