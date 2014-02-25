19b0dab2d063b5a1ebc54f8c5a59fe05563429ad
Lisa-Villeneuve <lisav@Lisa-Villeneuves-MacBook-Pro.local>
2014-02-25 12:09:32 -0500

    fixed Jekyll and ids
    Thomas is awesome

--- 2014/js/profile.js
+++ 2014/js/profile.js
@@ -1,94 +1,93 @@
-$(document).ready(function(){
-  var openStudent = 'student-section';
-  var openThumbnails = 'thumbnails';
-  var openItem = 'item';
-  var openProfile = null;
+$('.profile').hide();
 
-  $('.profile').hide();
-
-  $('#student-section').on('click', '.student-section-btn', function () {
-    var id = $(this).data('id');
-    openProfile = id;
-    $('#student-section').fadeOut(400);
-    $('#' + id).fadeIn({
-      duration: 800,
-      start: function () {
-        $('#student-section-wrap').height($('#' + id).outerHeight());
-      }
-    });
+//Open Profile
+$('#student-section').on('click', '.student-section-btn', function () {
+  var id = $(this).data('id');
+  $('#student-section').fadeOut(400);
+  $('#' + id).fadeIn({
+    duration: 800,
+    start: function () {
+     // $('#student-section-wrap').height($('#' + id).outerHeight());
+    }
   });
-
-  $('.btn-close-profile').click(function () {
-    $(this).parents('.profile').fadeOut(400);
-    openStudent = 'student-section';
-    $('#student-section').fadeIn({
-      duration: 400,
-      start: function () {
-        //$('#student-section-wrap').height($('#student-section').outerHeight());
-      }
-    });
-  });
-
-
-  $('.item').hide();
-
-  $('.thumbnails').on('click', '.thumbnail-btn', function () {
-    var id = $(this).data('id');
-    openItem = id;
-    $(this).parents('.thumbnails').fadeOut(400);
-    $('#' + id).fadeIn({
-      duration: 800,
-      start: function () {
-        //$('#thumbnails-wrap').height($('#' + id).outerHeight());
-      }
-    });
-  });
-
-  $('.btn-close-item').click(function () {
-    $(this).parents('.item').fadeOut(400);
-    openThumbnails = 'thumbnails';
-    $('#thumbnails').fadeIn({
-      duration: 400,
-      start: function () {
-        $('#thumbnails-wrap').height($('#thumbnails-section').outerHeight());
-      }
-    });
-  });
-
-
-
-  $('.next-btn-profile').click(function () {
-    var $workDiv = $(this).parents('.work-div');
-    var $nextDiv = $(this).parents('.work-div').next('.work-div');
-    var id = $nextDiv.attr('id');
-    openWork = id;
-
-    $workDiv.fadeOut(400);
-    $nextDiv.fadeIn({
-      duration: 400,
-      start: function () {
-        $('#work-wrap').height($('#' + id).outerHeight());
-      }
-    });
-  });
-
-  $('.prev-btn-profile').click(function() {
-    var $workDiv = $(this).parents('.work-div');
-    var $prevDiv = $(this).parents('.work-div').prev('.work-div');
-    var id = $prevDiv.attr('id');
-    openWork = id;
-
-    $workDiv.fadeOut(400);
-    $prevDiv.fadeIn({
-      duration: 400,
-      start: function () {
-        $('#work-wrap').height($('#' + id).outerHeight());
-      }
-    });
-  });
-
-  $(window).on('resize', function () {
-    $('#work-wrap').height($('#' + openWork).outerHeight());
-  });
-
 });
+
+// Close Profile
+$('.btn-close-profile').click(function () {
+  $(this).parents('.profile').fadeOut(400);
+  $('#student-section').fadeIn({
+    duration: 400,
+    start: function () {
+      //$('#student-section-wrap').height($('#student-section').outerHeight());
+    }
+  });
+});
+
+
+$('.item').hide();
+
+// Open Thumbnail
+
+$('.thumbnails').on('click', '.thumbnail-btn', function () {
+  var id = $(this).data('id');
+  $(this).parents('.thumbnails').fadeOut(400);
+  $('#' + id).fadeIn({
+    duration: 800,
+    start: function () {
+      //$('#thumbnails-wrap').height($('#' + id).outerHeight());
+    }
+  });
+});
+
+//Close Thumbnail
+
+$('.btn-close-item').click(function () {
+	var id = $(this).data('id');
+  $(this).parents('.item').fadeOut(400);
+  $('#' + id).fadeIn({
+    duration: 400,
+    start: function () {
+      //$('#thumbnails-wrap').height($('#thumbnails-section').outerHeight());
+    }
+  });
+});
+
+/*
+// Next profile button
+
+$('.next-btn-profile').click(function () {
+  var $workDiv = $(this).parents('.work-div');
+  var $nextDiv = $(this).parents('.work-div').next('.work-div');
+  var id = $nextDiv.attr('id');
+  openWork = id;
+
+  $workDiv.fadeOut(400);
+  $nextDiv.fadeIn({
+    duration: 400,
+    start: function () {
+      $('#work-wrap').height($('#' + id).outerHeight());
+    }
+  });
+});
+
+// Previous profile button
+
+$('.prev-btn-profile').click(function() {
+  var $workDiv = $(this).parents('.work-div');
+  var $prevDiv = $(this).parents('.work-div').prev('.work-div');
+  var id = $prevDiv.attr('id');
+  openWork = id;
+
+  $workDiv.fadeOut(400);
+  $prevDiv.fadeIn({
+    duration: 400,
+    start: function () {
+      $('#work-wrap').height($('#' + id).outerHeight());
+    }
+  });
+});*/
+
+$(window).on('resize', function () {
+  $('#work-wrap').height($('#' + openWork).outerHeight());
+});
+
