a5cd66b1c6fd894b54bdcc94327ab77099c41502
Winnie Lau <winlaud@gmail.com>
2014-02-27 12:46:18 -0500

    video openable

--- 2014/js/video.js
+++ 2014/js/video.js
@@ -3,7 +3,7 @@ var $placeholder = $('#video-placeholder');
 
  dialogPolyfill.registerDialog($dialog.get(0));
 
-$('#btn-open-video').on('click', function(){
+$('#btn-open').on('click', function(){
   $placeholder.html('<iframe src="http://player.vimeo.com/video/84420476?color=c9ff23&amp;autoplay=1" width="240" height="135" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
 
   /*.get() returns native javascript for us to access when also using jquery
@@ -14,7 +14,7 @@ $('#btn-open-video').on('click', function(){
   //showModal() --> Single dialog, with no user interaction behind
 });
 
-$('#close-btn-video').on('click', function(){
+$('#close-btn').on('click', function(){
   $dialog.get(0).close();
   $placeholder.html('');
 });
