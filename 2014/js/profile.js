c7db0e801de74098cb1ceff048e5d90326a89e9e
Thomas J Bradley <hey@thomasjbradley.ca>
2014-03-07 20:40:20 -0500

    Added lazy loading to the portfolio images to increase performance.

--- 2014/js/profile.js
+++ 2014/js/profile.js
@@ -1,26 +1,27 @@
 $(".profile img").error(function(){
-  $(this).hide();
+	$(this).hide();
 });
 
 $('.profile').hide();
 
 //Open Profile
 $('#student-section').on('click', '.student-section-btn', function (e) {
-  var id = $(this).data('id');
-  e.preventDefault();
-  $('#student-section').fadeOut(400);
-window.scrollTo(0, $('#profile-scroll-to-position').offset().top - $('.masthead').outerHeight() - 8);
-  $('#' + id).fadeIn({
-    duration: 800,
-  });
+	var id = $(this).data('id');
+	e.preventDefault();
+	$('#student-section').fadeOut(400);
+	$('#' + id + ' .img-lazy').trigger('trigger-image-load');
+	window.scrollTo(0, $('#profile-scroll-to-position').offset().top - $('.masthead').outerHeight() - 8);
+	$('#' + id).fadeIn({
+		duration: 800,
+	});
 });
 
 // Close Profile
 $('.btn-close-profile').click(function () {
-  $(this).parents('.profile').fadeOut(400);
-  $('#student-section').fadeIn({
-    duration: 800,
-  });
+	$(this).parents('.profile').fadeOut(400);
+	$('#student-section').fadeIn({
+		duration: 800,
+	});
 });
 
 
@@ -29,82 +30,84 @@ $('.item').hide();
 // Open Thumbnail
 
 $('.thumbnails').on('click', '.thumbnail-btn', function (e) {
-  var id = $(this).data('id');
-  e.preventDefault();
-  $(this).parents('.thumbnails').fadeOut(400);
-window.scrollTo(0, $('#profile-scroll-to-position').offset().top - $('.masthead').outerHeight() - 8);
-  $('#' + id).fadeIn({
-    duration: 800,
-  });
+	var id = $(this).data('id');
+	e.preventDefault();
+	$(this).parents('.thumbnails').fadeOut(400);
+	window.scrollTo(0, $('#profile-scroll-to-position').offset().top - $('.masthead').outerHeight() - 8);
+	$('#' + id).fadeIn({
+		duration: 800,
+	});
 });
 
 //Close Thumbnail
 
 $('.btn-close-item').click(function () {
-  var id = $(this).data('id');
-  $(this).parents('.item').fadeOut(400);
-  $('#' + id).fadeIn({
-    duration: 800,
-  });
+	var id = $(this).data('id');
+	$(this).parents('.item').fadeOut(400);
+	$('#' + id).fadeIn({
+		duration: 800,
+	});
 });
 
 $('.btn-close-item-bottom').click(function (e) {
-  var id = $(this).data('id');
-  e.preventDefault();
-  $(this).parents('.item').fadeOut(400);
-window.scrollTo(0, $('#profile-scroll-to-position').offset().top - $('.masthead').outerHeight() - 8);
-  $('#' + id).fadeIn({
-    duration: 800,
-  });
+	var id = $(this).data('id');
+	e.preventDefault();
+	$(this).parents('.item').fadeOut(400);
+	window.scrollTo(0, $('#profile-scroll-to-position').offset().top - $('.masthead').outerHeight() - 8);
+	$('#' + id).fadeIn({
+		duration: 800,
+	});
 });
 
 
 // Next profile button
 
 $('.btn-next-profile').click(function () {
-  var $workDiv = $(this).parents('.profile');
-  var $nextDiv = $(this).parents('.profile').next('.profile');
-  var id = $nextDiv.attr('id');
-  openWork = id;
+	var $workDiv = $(this).parents('.profile');
+	var $nextDiv = $(this).parents('.profile').next('.profile');
 
-  if ($nextDiv.length <= 0) {
-  	$nextDiv = $(this).parents('.profile').siblings('.profile').first();
-  }
+	if ($nextDiv.length <= 0) {
+		$nextDiv = $(this).parents('.profile').siblings('.profile').first();
+	}
 
-  $workDiv.fadeOut(400);
-  $nextDiv.fadeIn({
-    duration: 400,
-    start: function () {
-      //$('#student-section').height($('#' + id).outerHeight());
-    }
-  });
+	var id = $nextDiv.attr('id');
+
+	$workDiv.fadeOut(400);
+	$('#' + id + ' .img-lazy').trigger('trigger-image-load');
+	$nextDiv.fadeIn({
+		duration: 400,
+		start: function () {
+			//$('#student-section').height($('#' + id).outerHeight());
+		}
+	});
 });
 
 // Previous profile button
 
 $('.btn-prev-profile').click(function() {
-  var $workDiv = $(this).parents('.profile');
-  var $prevDiv = $(this).parents('.profile').prev('.profile');
-  var id = $prevDiv.attr('id');
-  openWork = id;
+	var $workDiv = $(this).parents('.profile');
+	var $prevDiv = $(this).parents('.profile').prev('.profile');
 
-  if ($prevDiv.length <= 0) {
-  	$prevDiv = $(this).parents('.profile').siblings('.profile').last();
-  }
+	if ($prevDiv.length <= 0) {
+		$prevDiv = $(this).parents('.profile').siblings('.profile').last();
+	}
 
-  $workDiv.fadeOut(400);
-  $prevDiv.fadeIn({
-    duration: 400,
-    start: function () {
-      //$('#student-section').height($('#' + id).outerHeight());
-    }
-  });
+	var id = $prevDiv.attr('id');
+
+	$workDiv.fadeOut(400);
+	$('#' + id + ' .img-lazy').trigger('trigger-image-load');
+	$prevDiv.fadeIn({
+		duration: 400,
+		start: function () {
+			//$('#student-section').height($('#' + id).outerHeight());
+		}
+	});
 });
 
 
 /*
 $(window).on('resize', function () {
-  $('#student-section').height($('#' + openWork).outerHeight());
+	$('#student-section').height($('#' + openWork).outerHeight());
  });
 
 */
