aa1af46a5e500c629c37b44e2a09ef9a425300aa
Lisa-Villeneuve <lisav@Lisa-Villeneuves-MacBook-Pro.local>
2014-03-10 23:33:00 -0400

    attempted the integration of iframe popup

--- 2014/js/video.js
+++ 2014/js/video.js
@@ -1,23 +1,40 @@
-var $dialog = $('#video');
-var $placeholder = $('#video-placeholder');
+// var $dialog = $('#video');
+// var $placeholder = $('#video-placeholder');
 
- dialogPolyfill.registerDialog($dialog.get(0));
+//  dialogPolyfill.registerDialog($dialog.get(0));
 
-$('#btn-open').on('click', function(){
-  $placeholder.html('<iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+// $('#btn-open').on('click', function(){
+//   $placeholder.html('<iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
 
-  /*.get() returns native javascript for us to access when also using jquery
-  (there is both a show for jquery and javascript with different functions)*/
+//   $dialog.get(0).show();
 
-  $dialog.get(0).show();
-  //show() --> allows multiple dialogs
-  //showModal() --> Single dialog, with no user interaction behind
+// });
 
+// $('#close-btn').on('click', function(){
+//   $dialog.get(0).close();
+//   $placeholder.html('');
+// });
+
+$(document).ready(function() {
+	$('.video').magnificPopup({
+		type: 'iframe',
+
+
+		iframe: {
+		  markup: '<div class="mfp-iframe-scaler">'+
+		            '<div class="mfp-close"></div>'+
+		            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+		          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+		  patterns: {
+		    vimeo: {
+		      index: 'vimeo.com/',
+		      id: '85752664/',
+		      src: '//player.vimeo.com/video/%id%?autoplay=1'
+		    },
+		  },
+
+		  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+		}
 
 });
-
-$('#close-btn').on('click', function(){
-  $dialog.get(0).close();
-  $placeholder.html('');
-});
-
