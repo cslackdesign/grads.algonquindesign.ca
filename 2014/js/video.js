7cea0381b675e4681a546406e510705be46f1090
Lisa-Villeneuve <lisav@Lisa-Villeneuves-MacBook-Pro.local>
2014-03-10 23:43:27 -0400

    tried the video with inline popup and it almost works

--- 2014/js/video.js
+++ 2014/js/video.js
@@ -15,26 +15,35 @@
 //   $placeholder.html('');
 // });
 
-$(document).ready(function() {
-	$('.video').magnificPopup({
-		type: 'iframe',
+// $(document).ready(function() {
+// 	$('.video').magnificPopup({
+// 		type: 'iframe',
 
 
-		iframe: {
-		  markup: '<div class="mfp-iframe-scaler">'+
-		            '<div class="mfp-close"></div>'+
-		            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-		          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+// 		iframe: {
+// 		  markup: '<div class="mfp-iframe-scaler">'+
+// 		            '<div class="mfp-close"></div>'+
+// 		            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+// 		          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
 
-		  patterns: {
-		    vimeo: {
-		      index: 'vimeo.com/',
-		      id: '85752664/',
-		      src: '//player.vimeo.com/video/%id%?autoplay=1'
-		    },
-		  },
+// 		  patterns: {
+// 		    vimeo: {
+// 		      index: 'vimeo.com/',
+// 		      id: '85752664/',
+// 		      src: '//player.vimeo.com/video/%id%?autoplay=1'
+// 		    },
+// 		  },
 
-		  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-		}
+// 		  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+// 		}
 
+// });
+
+
+$('button').magnificPopup({
+  items: {
+      src: '<div class="white-popup"><iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>',
+      type: 'inline'
+  },
+  closeBtnInside: true
 });
