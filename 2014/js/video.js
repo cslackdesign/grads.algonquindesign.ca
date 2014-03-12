b5cefdbdaba8239ddd910dad18e897f051d79957
Cassie Slack <cslackdesign@gmail.com>
2014-03-12 12:12:04 -0400

    Made all the things :)
    Added Vimeo Video, changes skills to icons

--- 2014/js/video.js
+++ 2014/js/video.js
@@ -18,7 +18,7 @@
 
 $('.btn-open').magnificPopup({
   items: {
-      src: '<div class="white-popup"><iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><h2>Welcome to Level 14</h2></div>',
+      src: '<div class="white-popup"><iframe class="vid" src="//player.vimeo.com/video/88831821?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><h2>Welcome to Level 14</h2></div>',
       type: 'inline'
   },
   closeBtnInside: true
