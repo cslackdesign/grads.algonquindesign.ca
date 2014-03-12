608098a7001b6cd74988103803c46d7a1ee19a37
Cassie Slack <cslackdesign@gmail.com>
2014-03-11 21:57:43 -0400

    Adding Description

--- 2014/js/video.js
+++ 2014/js/video.js
@@ -18,7 +18,7 @@
 
 $('.btn-open').magnificPopup({
   items: {
-      src: '<div class="white-popup"><iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><h2>Insert some text here</h2></div>',
+      src: '<div class="white-popup"><iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><h2>Welcome to Level 14</h2></div>',
       type: 'inline'
   },
   closeBtnInside: true
