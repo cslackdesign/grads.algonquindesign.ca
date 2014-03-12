fe4f61f9b4e56b042da3a24610c8ceaf47617872
Cassie Slack <cslackdesign@gmail.com>
2014-03-11 20:32:19 -0400

    Merge branch 'gh-pages' of https://github.com/cslackdesign/grads.algonquindesign.ca into gh-pages
    Conflicts:
    _includes/css/general.css

--- 2014/js/video.js
+++ 2014/js/video.js
@@ -1,23 +1,25 @@
-var $dialog = $('#video');
-var $placeholder = $('#video-placeholder');
+// var $dialog = $('#video');
+// var $placeholder = $('#video-placeholder');
 
- dialogPolyfill.registerDialog($dialog.get(0));
+//  dialogPolyfill.registerDialog($dialog.get(0));
 
-$('#btn-open').on('click', function(){
-  $placeholder.html('<iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+// $('#btn-open').on('click', function(){
+//   $placeholder.html('<iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
 
-  /*.get() returns native javascript for us to access when also using jquery
-  (there is both a show for jquery and javascript with different functions)*/
+//   $dialog.get(0).show();
 
-  $dialog.get(0).show();
-  //show() --> allows multiple dialogs
-  //showModal() --> Single dialog, with no user interaction behind
+// });
+
+// $('#close-btn').on('click', function(){
+//   $dialog.get(0).close();
+//   $placeholder.html('');
+// });
 
 
+$('.btn-open').magnificPopup({
+  items: {
+      src: '<div class="white-popup"><iframe class="vid" src="//player.vimeo.com/video/85752664?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><h2>Insert some text here</h2></div>',
+      type: 'inline'
+  },
+  closeBtnInside: true
 });
-
-$('#close-btn').on('click', function(){
-  $dialog.get(0).close();
-  $placeholder.html('');
-});
-
